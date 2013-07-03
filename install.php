2ebbde6550126e0d84fd3bca4b57959d90a13f47
Spittie <spittie@localhost.localdomain>
2013-07-03 21:54:07 +0200

    PostgreSQL 9.1 and newer supported, won't add support for older versions

--- install.php
+++ install.php
@@ -25,39 +25,10 @@ if ($db_type == 'sqlite') {
 
 	$pg_version = $db->getAttribute(PDO::ATTR_CLIENT_VERSION);
 	if ($pg_version < 9) {
-	// PostgreSQL 8.4 (Debian 6 default) support
-	// http://stackoverflow.com/questions/1766046/postgresql-create-table-if-not-exists
-		$sql = "CREATE OR REPLACE FUNCTION create_mytable ()
-			RETURNS void AS
-			$_$
-			BEGIN
-
-			IF EXISTS (
-				SELECT *
-				FROM   pg_catalog.pg_tables 
-				WHERE  schemaname = 'myschema'
-				AND    tablename  = 'mytable'
-				) THEN
-			RAISE NOTICE 'Table already exists.';
-			ELSE
-			CREATE TABLE myschema.mytable (
-					id int(1) NOT NULL AUTO_INCREMENT,
-					name varchar(65) NOT NULL,
-					url varchar(65) NOT NULL,
-					location varchar(65) NOT NULL,
-					host varchar(65) NOT NULL,
-					type varchar(65) NOT NULL,
-					PRIMARY KEY (id)
-			);
-			END IF;
-
-			END;
-			$_$ LANGUAGE plpgsql;
-			SELECT create_mytable(); "
+		die("PostgreSQL 8.4 and older are not supported");
 	} else {
 		$sql = "CREATE TABLE IF NOT EXISTS servers (
-			id int(1) NOT NULL AUTO_INCREMENT,
-			name varchar(65) NOT NULL,
+			id SERIAL, name varchar(65) NOT NULL,
 			url varchar(65) NOT NULL,
 			location varchar(65) NOT NULL,
 			host varchar(65) NOT NULL,
