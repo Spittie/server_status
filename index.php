7ca82b1d6cf44856d2f6956b203d907ed03363c9
Spittie <spittie@localhost.localdomain>
2013-07-03 18:24:50 +0200

    ops

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 include('./includes/config.php');
 
-echo 'a';
+$sJavascript = "";
+$sTable = "";
 
 try{
 	$query = $db->prepare("SELECT * FROM servers ORDER BY id");
@@ -10,38 +11,37 @@ try{
 catch(PDOException $e){
     die('Query failed: ' . $e->getMessage());
 }
-
 	$sJavascript .= '<script type="text/javascript">
 		function uptime() {
 			$(function() {';
 	
 while($result = $query->fetchObject()){
-	$sJavascript .= '$.getJSON("pull/index.php?url='.$result["id"].'",function(result){
-	$("#online'.$result["id"].'").html(result.online);
-	$("#uptime'.$result["id"].'").html(result.uptime);
-	$("#load'.$result["id"].'").html(result.load);
-	$("#memory'.$result["id"].'").html(result.memory);
-	$("#hdd'.$result["id"].'").html(result.hdd);
+	$sJavascript .= '$.getJSON("pull/index.php?url='.$result->id.'",function(result){
+	$("#online'.$result->id.'").html(result.online);
+	$("#uptime'.$result->id.'").html(result.uptime);
+	$("#load'.$result->id.'").html(result.load);
+	$("#memory'.$result->id.'").html(result.memory);
+	$("#hdd'.$result->id.'").html(result.hdd);
 	});';
 	$sTable .= '
 		<tr>
-			<td id="online'.$result["id"].'">
+			<td id="online'.$result->id.'">
 				<div class="progress">
 					<div class="bar bar-danger" style="width: 100%;"><small>Down</small></div>
 				</div>
 			</td>
-			<td>'.$result["name"].'</td>
-			<td>'.$result["type"].'</td>
-			<td>'.$result["host"].'</td>
-			<td>'.$result["location"].'</td>
-			<td id="uptime'.$result["id"].'">n/a</td>
-			<td id="load'.$result["id"].'">n/a</td>
-			<td id="memory'.$result["id"].'">
+			<td>'.$result->name.'</td>
+			<td>'.$result->type.'</td>
+			<td>'.$result->host.'</td>
+			<td>'.$result->location.'</td>
+			<td id="uptime'.$result->id.'">n/a</td>
+			<td id="load'.$result->id.'">n/a</td>
+			<td id="memory'.$result->id.'">
 				<div class="progress progress-striped active">
 					<div class="bar bar-danger" style="width: 100%;"><small>n/a</small></div>
 				</div>
 			</td>
-			<td id="hdd'.$result["id"].'">
+			<td id="hdd'.$result->id.'">
 				<div class="progress progress-striped active">
 					<div class="bar bar-danger" style="width: 100%;"><small>n/a</small></div>
 				</div>
@@ -54,6 +54,5 @@ while($result = $query->fetchObject()){
 	uptime();
 	setInterval(uptime, '.$sSetting['refresh'].');
 	</script>';
-	
 include('./templates/default/index.php');
 ?>
